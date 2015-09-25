b6762f64001291bd8994f8b61ebb276eb9f94429
Jiawei <64341393@qq.com>
2015-09-25 10:01:25 +0800

    添加一些注释

--- src/donson/report/StreamingReport.scala
+++ src/donson/report/StreamingReport.scala
@@ -1,13 +1,14 @@
 package donson.report
 
-import java.util.Properties
-
-import donson.common.{SetApplicationLoggerLevel, ReportUtils}
+import com.mongodb.hadoop.MongoOutputFormat
+import donson.common.{ReportUtils, SetApplicationLoggerLevel}
+import org.apache.hadoop.conf.Configuration
 import org.apache.log4j.Level
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.bson.BasicBSONObject
 
 object StreamingReport {
 
@@ -23,46 +24,56 @@ object StreamingReport {
     val group = "test-group"
     val topics = "testTopic1"
     val numThreads = 1
-
-    // 配置数据库连接参数
-    val connectionProperties = new Properties()
-    connectionProperties.setProperty("user", "root")
-    connectionProperties.setProperty("password", "123456")
-    connectionProperties.setProperty("driver", "com.mysql.jdbc.Driver")
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
 
     val sparkConf = new SparkConf().setAppName("StreamingSQL").setMaster("local[2]") //spark://cluster01:7077
     val ssc = new StreamingContext(sparkConf, Seconds(60))
     /*ssc.checkpoint("hdfs://master:9000/ck6")*/
 
-    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+
+    val mongoConf = new Configuration()
+    //  mongoConf.set("mongo.input.uri","mongodb://192.168.1.253:27017/db.input")
+    mongoConf.set("mongo.output.uri","mongodb://192.168.1.253:27017/db.output4") // 写入db的output2中
 
     val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
     lines.filter(_.split(",").length >= 45).foreachRDD { rdd =>
       val sqlContext = SQLContext.getOrCreate(rdd.sparkContext)
       val schema = ReportUtils.makeSchemaInfomentions
-
       val rowRDD = rdd.map(_.split(",")).map(ReportUtils.makeDataToObject)
-      val rddDataFrame = sqlContext.createDataFrame(rowRDD, schema)
+      val adInfoTable = sqlContext.createDataFrame(rowRDD, schema)
+
+      adInfoTable.registerTempTable("adloginfo")
+      sqlContext.cacheTable("adloginfo")
+
+      // 报表 DSP_AssHourlyDetailDataReport 示例
+      val results = sqlContext.sql(ReportUtils.DSP_AssHourlyDetailDataReport)
+      val saveRDD  = results.map(
+        rdata => {
+          val bson = new BasicBSONObject()
+          bson.put("AdvertisersID", rdata.getAs[String]("AdvertisersID"))
+          bson.put("ADOrderID", rdata.getAs[String]("ADOrderID"))
+          bson.put("ADCreativeID", rdata.getAs[String]("ADCreativeID"))
+          bson.put("ReqDate", rdata.getAs[String]("ReqDate"))
+          bson.put("ReqHour", rdata.getAs[String]("ReqHour"))
+          bson.put("IsShowSum", rdata.getAs[String]("IsShowSum"))
+          bson.put("IsClickSum", rdata.getAs[String]("IsClickSum"))
+          bson.put("IsTakeBidSum", rdata.getAs[String]("IsTakeBidSum"))
+          bson.put("IsSuccessBidSum", rdata.getAs[String]("IsSuccessBidSum"))
+          bson.put("PaymentSum", rdata.getAs[String]("PaymentSum"))
+          bson.put("costSum", rdata.getAs[String]("costSum"))
+          bson.put("IndependentAudienceSum", rdata.getAs[String]("IndependentAudienceSum"))
+          bson.put("IndependentAudienceclickAudienceSum", rdata.getAs[String]("IndependentAudienceclickAudienceSum"))
+          (null, bson)
+      })
+      // file:///bogus无实际意义
+      saveRDD.saveAsNewAPIHadoopFile("file:///bogus", classOf[Any], classOf[Any], classOf[MongoOutputFormat[Any, Any]], mongoConf)
 
 
-      rddDataFrame.write.parquet("hdfs://192.168.1.220:9000/dshbase/report_ch_" + ReportUtils.formateFileName + ".parquet")
+      /**
+       * other report code
+       */
 
-
-     /* val parquetFile = sqlContext.read.parquet("hdfs://192.168.1.220:9000/donson/streaming/report_1_" + ReportUtils.formateFileName + ".parquet")
-      parquetFile.registerTempTable("adloginfo")
-      // cache table
-//      sqlContext.cacheTable("adloginfo")
-
-
-      // 查询结果并存储到数据库中
-      val results = sqlContext.sql("SELECT AdvertisersID,ADOrderID,ADCreativeID,ReqDate,ReqHour,sum(IsShow),sum(IsClick),sum(IsTakeBid),sum(IsSuccessBid)," +
-        "sum(Payment),sum(cost),sum(IndependentAudience),sum(IndependentAudienceclickAudience) " +
-        "FROM adloginfo " +
-        "group by AdvertisersID,ADOrderID,ADCreativeID,ReqDate,ReqHour")
-      // results.collect().foreach(println)
-      results.write.mode(SaveMode.Append).jdbc("jdbc:mysql://192.168.1.130:3306/test", "report_1", connectionProperties)*/
-      // uncache table
-//      sqlContext.uncacheTable("adloginfo")
+      sqlContext.uncacheTable("adloginfo")
     }
 
     ssc.start()
